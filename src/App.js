a25e465ae25dad637d31060177bce754e6934089
Scott Ings <scott.m.ings@gmail.com>
2020-04-12 15:50:52 -0700

    working search page

--- src/App.js
+++ src/App.js
@@ -9,14 +9,15 @@ import * as BooksAPI from './BooksAPI'
 
 class BooksApp extends React.Component {
   state = {
-    books: []
+    books: [],
+    searchBooks: []
   }
   componentDidMount() {
     BooksAPI.getAll()
       .then((books) => {
         // console.log(books)
         this.setState(() => ({
-          books
+          books: books
         }))
       })
   }
@@ -44,12 +45,50 @@ class BooksApp extends React.Component {
 
   addBook = (book,shelf) => {
     BooksAPI.update(book,shelf)
-      .then((book) => {
-        this.setState((currentState) => ({
-          books: currentState.books.concat([book])
-        }))
+      .then(() => {
+        this.setState((currentState) => {
+          book.shelf = shelf
+          return {
+            books: currentState.books.concat([book])
+          }
+        })
       })
   }
+
+
+  search = (searchQuery) => {
+    // console.log(searchQuery)
+    if (searchQuery === '') {
+      this.setState(() => ({
+        searchBooks: []
+      }))
+    } else {
+    BooksAPI.search(searchQuery)
+      .then((searchBooks) => {
+        // console.log(searchBooks)
+        if (searchBooks.error) {
+          this.setState(() => ({
+            searchBooks: []
+          }))
+        } else {
+          this.setState(() => {
+            const results = searchBooks.filter((b) => b.imageLinks)
+            results.map((r) => {
+              const existingBook = this.state.books.find((bo) => bo.id === r.id)
+              if (existingBook) {
+                r.shelf = existingBook.shelf
+              }
+              return r
+            })
+            return {
+              searchBooks: results
+            }
+          })
+        }
+      })
+    }
+  }
+
 
   render() {
     return (
@@ -62,6 +101,9 @@ class BooksApp extends React.Component {
         )} />
         <Route path='/search' render={() => (
           <SearchBooks
+            results={this.state.searchBooks}
+            onAddBook={this.addBook}
+            onSearch={this.search}
           />
         )} />
       </div>
--- src/SearchBooks.js
+++ src/SearchBooks.js
@@ -1,19 +1,19 @@
 import React, { Component } from 'react'
 import { Link } from 'react-router-dom'
+import Book from './Book'
 
 
 class SearchBooks extends Component {
 
 	render() {
+		const { results, onAddBook, onSearch } = this.props
 
 		return (
           <div className="search-books">
             <div className="search-books-bar">
             <Link to="/">
               <button className="close-search">
-              	
               		Close
-          
               </button>
               </Link>
               <div className="search-books-input-wrapper">
@@ -25,12 +25,18 @@ class SearchBooks extends Component {
                   However, remember that the BooksAPI.search method DOES search by title or author. So, don't worry if
                   you don't find a specific author or title. Every search is limited by search terms.
                 */}
-                <input type="text" placeholder="Search by title or author"/>
+                <input onChange={(event) => onSearch(event.target.value)} type="text" placeholder="Search by title or author"/>
 
               </div>
             </div>
             <div className="search-books-results">
-              <ol className="books-grid"></ol>
+              <ol className="books-grid">
+                {results.map((book) => (
+                  <li key={book.id}>
+                    <Book book={book} onUpdateBook={onAddBook} />
+                  </li>
+                ))}   
+              </ol>
             </div>
           </div>
          )
